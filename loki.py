e55bf32e636acf4b796051a7964cafea5bef3c14
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-10-05 01:10:20 -0700

    Created argument parser

--- loki.py
+++ loki.py
@@ -1,5 +1,7 @@
+import argparse
 import pathlib
 import tqdm
+import os
 
 def move(original: pathlib.Path, new: pathlib.Path, chunk_size: int) -> None:
     with original.open(mode='rb') as old_file, new.open(mode='wb') as new_file, \
@@ -9,3 +11,50 @@ def move(original: pathlib.Path, new: pathlib.Path, chunk_size: int) -> None:
             progress.update(len(chunk))
 
     original.unlink()
+
+parser = argparse.ArgumentParser(description='Use this configure the Chia Plotting process.')
+parser.add_argument(
+    'count',
+    help='Number of plots to create'
+)
+parser.add_argument(
+    'temp-1',
+    type=pathlib.Path,
+    dest='temp_1',
+    help='Temporary directory, needs ~220 GiB'
+)
+parser.add_argument(
+    'finaldir',
+    type=pathlib.Path,
+    help='Final directory to move to.'
+)
+parser.add_argument(
+    '-2', '--temp-2',
+    type=pathlib.Path,
+    dest='temp_2',
+    help='Temporary directory 2, needs ~110 GiB [RAM] (default =<temp-1>)'
+)
+parser.add_argument(
+    '-3', '--temp-3',
+    type=pathlib.Path,
+    dest='temp_3',
+    help='Where to write plots to.'
+)
+parser.add_argument(
+    '-t', '--threads',
+    type=int,
+    help='Number of threads',
+    default=os.cpu_count()
+)
+parser.add_argument(
+    '-p', '--poolkey',
+    help='Pool Public Key (48 bytes)',
+    default=os.environ.get('pool-key')
+)
+parser.add_argument(
+    '-f', '--farmerkey',
+    help='Farmer Public Key (48 bytes)',
+    default=os.environ.get('farmer-key')
+)
+
+config = parser.parse_args()
