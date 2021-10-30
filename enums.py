288c038ad13eda85d6b95bd020c8667f090b3012
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-10-30 15:08:08 -0700

    Created enums.py

---
+++ enums.py
@@ -0,0 +1,29 @@
+from enum import Enum, auto
+
+
+class Manufacturer(Enum):
+    seagate = auto()
+    samsung = auto()
+    western_digital = auto()
+    toshiba = auto()
+    hitachi = auto()
+
+
+class StorageType(Enum):
+    hdd = auto()
+    ssd = auto()
+    hybrid = auto()
+
+
+class FormFactor(Enum):
+    two_point_five = auto()
+    three_point_five = auto()
+    usb = auto()
+    other = auto()
+
+
+class Contributors(Enum):
+    andres = auto()
+    ryan = auto()
+    dylan = auto()
+    jane = auto()
