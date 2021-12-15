605c24d9a78dd501a1246b31013da38febcdfaa3
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-12-14 21:22:22 -0800

    Completely changed how the program works.
    Now it used a pipe to read info.  This is the configuration needed to run the program:
    chia_plot PARAMS-HERE | python3 loki.py PARAMS-HERE

--- loki.py
+++ loki.py
@@ -1,116 +1,74 @@
-import subprocess
-import threading
 import argparse
+import threading
 import datetime
 import pathlib
-import dotenv
+import time
 import tqdm
 import sys
-import os
-import io
 import re
 
-dotenv.load_dotenv()
-
-def move(original: pathlib.Path, new: pathlib.Path, chunk_size: int = 65_536) -> None:
-    with original.open(mode='rb') as old_file, new.open(mode='wb') as new_file, \
-    tqdm.tqdm(total=original.stat().st_size, unit_scale=True, unit='b') as progress:
+def move(original: pathlib.Path, new: pathlib.Path, progress: tqdm.tqdm, chunk_size: int = 65_536) -> None:
+    with original.open(mode='rb') as old_file, (new / original.name).open(mode='wb') as new_file:
         for chunk in iter(lambda: old_file.read(chunk_size), b''):
             new_file.write(chunk)
             progress.update(len(chunk))
+            time.sleep(2)
 
     original.unlink()
+    progress.close()
 
-parser = argparse.ArgumentParser(description='Use this configure the Chia Plotting process.')
+parser = argparse.ArgumentParser()
 parser.add_argument(
-    'count',
-    type=int,
-    help='Number of plots to create'
-)
-parser.add_argument(
-    'temp-1',
+    'location',
     type=pathlib.Path,
-    dest='temp_1',
-    help='Temporary directory, needs ~220 GiB'
+    help='Location to move the plot after it is created.'
 )
 parser.add_argument(
-    'finaldir',
+    '--log-file',
     type=pathlib.Path,
-    help='Final directory to move to.'
+    help='Location of the log file.',
+    default='chia.log',
+    dest='log_file'
 )
-parser.add_argument(
-    '-2', '--temp-2',
-    type=pathlib.Path,
-    dest='temp_2',
-    help='Temporary directory 2, needs ~110 GiB [RAM] (default =<temp-1>)'
-)
-parser.add_argument(
-    '-3', '--temp-3',
-    type=pathlib.Path,
-    dest='temp_3',
-    help='Where to write plots to.'
-)
-parser.add_argument(
-    '-t', '--threads',
-    type=int,
-    help='Number of threads',
-    default=os.cpu_count()
-)
-parser.add_argument(
-    '-p', '--poolkey',
-    help='Pool Public Key (48 bytes)',
-    default=os.environ.get('pool-key')
-)
-parser.add_argument(
-    '-f', '--farmerkey',
-    help='Farmer Public Key (48 bytes)',
-    default=os.environ.get('farmer-key')
-)
-
 config = parser.parse_args()
 
-class StoreAndStdout(io.TextIOWrapper):
-    def write(self, text):
-        with open('.log', 'a') as log:
-            log.write(text)
-        if not hasattr('self', '_text'):
-            self._text = ''
-        self._text += text
-        super().write(text)
+logs = ''
+plot_name = None
+progress_bar = None
+final_dir = None
+final_dir_re = re.compile(r"Final Directory: ((/[-\w ]+)+)")
+try:
+    for line in sys.stdin:
+        logs += line
+        tqdm.tqdm.write(line)
+        if final_dir is None:
+            if search := final_dir_re.search(line):
+                final_dir = search[1]
 
-    def read(self):
-        return self._text
+        if plot_name is None:
+            current_date = datetime.date.today()
+            plot_name = re.search(
+                fr"plot-k32-{format(current_date, '%Y-%m-%d')}-\d{{2}}-\d{{2}}-\w{{64}}", line
+            )
+            if plot_name:
+                plot_name = plot_name[0]
 
-chia_plotter = pathlib.Path('chia_plot').absolute()
-if not chia_plotter.exists():
-    raise FileNotFoundError('Could not find Chia Plotter binary.')
+        # This indicates that plot is finised being created and has been moved to the final directory
+        if line.startswith('Copy to'):
+            if plot_name is None:
+                raise FileNotFoundError('Plot name could not be identified')
+            elif final_dir is None:
+                raise FileNotFoundError('Final directory could not be identified')
+            plot = pathlib.Path(final_dir) / plot_name
+            if not plot.exists():
+                raise FileNotFoundError(f'Plot not found in expected location: {plot}')
 
-stream = StoreAndStdout(sys.stdout)
-for _ in range(config.count):
-    current_date = datetime.date.today()
-    plot = subprocess.Popen(
-        (f"/.{chia_plotter} -t {config.temp_1} -2 {config.temp_2} -d {config.temp_3} "
-        f"--threads {config.threads} -p {config.poolkey} -f {config.farmerkey}").split(),
-        stdout=stream,
-        stderr=stream,
-        text=True
-    )
-    plot.wait()
+            progress_bar = tqdm.tqdm(total=plot.stat().st_size, unit_scale=True, unit='b')
+            thread = threading.Thread(target=move, args=(plot, config.location, progress_bar))
+            thread.start()
 
-    plot_name = re.search(
-        fr"plot-k32-{format(current_date, '%Y-%m-%d')}-\d{{2}}-\d{{2}}-\w{{64}}",
-        stream.read()
-    )
-    if plot_name:
-        plot_name = plot_name[0]
-    else:
-        raise RuntimeError('Could not determine the name of the plot that was just created.')
-
-    thread = threading.Thread(
-        target=move,
-        args=(
-            config.temp_3 / plot_name,
-            config.finaldir / plot_name
-        )
-    )
-    thread.start()
+            plot_name = None
+            final_dir = None
+finally:
+    with config.log_file.open(mode='a') as file:
+        file.write(logs)
